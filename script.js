4a052b1a6289b21c9f01a6d3f4e68352f6330980
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-10-24 11:46:17 -0500

    added js code for listen for save button clicks and hourUpdater

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-$('#currentDay').text(moment().format('dddd, MMMM Do'));
+$(document).ready(function () {
+  $('saveBtn').on('click', function () {
+    var value = $(this).siblings('.description').val();
+    var time =(this).parent().attr('id');
+    //save to local storage
+    localStorage.setItem(time, value);
+  });
+
+  function hourUpdater() {
+    var currentHour = moment().hours();
+
+  }
+
+  hourUpdater();
+
+//current day is displayed at the top of the calendar
+  $('#currentDay').text(moment().format('dddd, MMMM Do'));
+});
